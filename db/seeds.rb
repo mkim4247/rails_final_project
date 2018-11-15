5c7b6d256b10055d14b86f997319a4961cc8e1cf
Ana Harris <vana1990@gmail.com>
2018-11-15 15:15:51 -0500

    added links for future navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 
+#
 # Comment.destroy_all
 # Follow.destroy_all
 # Like.destroy_all
